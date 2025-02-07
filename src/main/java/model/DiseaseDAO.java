7735b1afe858cef6e233104e119380b151e7a912
Aunsxm <tngus893716@gmail.com>
2025-02-07 15:08:08 +0900

    add : 모든질병정보 반환 메소드 추가

--- src/main/java/model/DiseaseDAO.java
+++ src/main/java/model/DiseaseDAO.java
@@ -1,5 +1,36 @@
 package model;
 
-public class DiseaseDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import model.domain.Disease;
+import util.DataSourceManager;
+
+public class DiseaseDAO {
+	//모든 질병 정보 반환 메소드
+	public static ArrayList<Disease> getAllDisease() throws Exception{
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Disease> all = null;
+		
+		try {
+			con = DataSourceManager.getConnection();
+			pstmt = con.prepareStatement("select * from diseases");
+			rs = pstmt.executeQuery();
+			
+			all = new ArrayList<>();
+			while(rs.next()) {
+				all.add(new Disease(rs.getInt("disease_id"), rs.getString("disease_name")));
+			}
+		}finally {
+			DataSourceManager.close(con,pstmt,rs);
+		}
+		return all;
+	}
+
+	
+	
 }
